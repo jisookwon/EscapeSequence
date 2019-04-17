110b78368a31aa353a9702d097a103d7c4c32e3d
jisookwon <jinskwon@hotmail.com>
2019-04-17 14:41:29 -0400

    First commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String asterisk8 = "********";
+        System.out.println(asterisk8+"\n"+"*\t   *"+"\n"+"*\t   *"+"\n"+"*\t   *"+"\n"+"*\t   *"+"\n"+asterisk8+"\n");
+        System.out.println("  ***"+"\n"+" *\t *"+"\n"+"*\t  *"+"\n"+"*\t  *"+"\n"+" *\t *"+"\n"+"  ***"+"\n"+"\r");
+        System.out.println("  *"+"\n"+" * *"+"\n"+"*\t*"+"\n"+" * *"+"\n"+"  *");
+    }
+}
